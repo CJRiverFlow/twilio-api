7c82141bc91865338b403bd9591550a430cd2d0d
Christian <cjosue.flores@gmail.com>
2021-08-06 10:46:20 -0700

    Empty message handling

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ CURRENTLY USING THIS ON WEBHOOK APP
 app.post('/voice', (request,response) =>{
     //Just to execute the WELCOME intent message
     console.log(`Inbound call`)
+    //console.log(request.body) 
+    console.log(request.body.CallSid) // Unique call Id
+    console.log(request.body.CallStatus) // Call status, changes between ringing, in-progress and completed.
+    
     response.type('text/xml');
     response.send(`
     <Response>
@@ -34,21 +38,33 @@ app.post('/loop', (request,response) =>{
     /*
     Loop process to speak-listen continouos flow.
     */
-    var message = request.body.SpeechResult
+    console.log(request.body.CallSid)
+    console.log(request.body.CallStatus)
+
+    if (typeof request.body.SpeechResult !== 'undefined'){
+        var message = request.body.SpeechResult
+    }else{
+        var message = "No pude escuchar nada, podría repetirlo?"
+    }
+
     console.log(message)
     response.type('text/xml');
     response.send(`
     <Response>
-        <Gather action="/loop" input='speech' language='es-UY' speechTimeout='auto'>
+        <Gather action="/loop" input='speech' language='es-UY' speechTimeout='auto' actionOnEmptyResult="true">
             <Say voice='Polly.Lupe-Neural'>
                 <prosody rate="90%">
-                    Tu mensaje fue: ${message}
+                    ${message}
                 </prosody>
             </Say>
+        <Redirect method="POST">
+            /loop
+        </Redirect>
         </Gather>
     </Response>
     `);
 });
+
 
 app.listen(port, () => {
     console.log(`Twilio app listening at http://localhost:${port}`)
